744afcc21aff33c1d93f72f3d3550c3a1c63caaf
Chechi07_ <chechi07gamer@gmail.com>
2022-03-10 12:51:43 +0000

    Initial commit

---
+++ scaling-meme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scaling_meme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
