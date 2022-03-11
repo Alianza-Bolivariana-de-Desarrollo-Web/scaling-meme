a75004d8e797f00112746200dbddcdecc99cced6
desmo13 <mariodesmo13@gmail.com>
2022-03-11 07:59:27 +0000

    2022/03/10
    acabado calcular edad en años

--- MMFunciones.cs
+++ MMFunciones.cs
@@ -6,5 +6,19 @@ namespace scaling_meme
 {
     class MMFunciones
     {
+        static int[] FechaActual = { 10, 04, 2022 };
+        /// <summary>
+        /// funcion que calcula los años entre 1 fecha dada y la actual
+        /// </summary>
+        /// <param name="fecha">array que contiene la fecha</param>
+        /// <returns></returns>
+        public static int CalcularEdadAños(int[] fecha)
+        {
+            const int posicionAño = 2; 
+             int año = fecha[posicionAño];
+            int Edad = año - FechaActual[posicionAño];
+
+            return Edad;
+        }
     }
 }
